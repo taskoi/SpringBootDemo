673ce4a11f9e57a83bac73bdb4265702289c071c
WF | Ivan Tasevski <wf-ivantasevski@wf-mariomisevski.local>
2019-05-03 16:00:46 +0200

    create user method created

--- src/main/java/com/webfactory/springbootdemo/demoproject/service/UserService.java
+++ src/main/java/com/webfactory/springbootdemo/demoproject/service/UserService.java
@@ -1,5 +1,6 @@
 package com.webfactory.springbootdemo.demoproject.service;
 
+import com.webfactory.springbootdemo.demoproject.model.User;
 import com.webfactory.springbootdemo.demoproject.persistance.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,5 +11,15 @@ public class UserService {
     @Autowired
     UserRepository userRepository;
 
+    public User createUser(User user){
 
+        User userNew = new User();
+        userNew.setEmail(user.getEmail());
+        userNew.setFirstName(user.getFirstName());
+        userNew.setLastName(user.getLastName());
+        userNew.setNickname(user.getNickname());
+        userNew.setPassword(user.getPassword());
+
+        return userRepository.save(userNew);
+    }
 }
--- src/main/java/com/webfactory/springbootdemo/demoproject/web/UserController.java
+++ src/main/java/com/webfactory/springbootdemo/demoproject/web/UserController.java
@@ -1,15 +1,26 @@
 package com.webfactory.springbootdemo.demoproject.web;
 
+import com.webfactory.springbootdemo.demoproject.model.User;
 import com.webfactory.springbootdemo.demoproject.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+
+@RestController
 public class UserController {
 
     @Autowired
     UserService userService;
 
-
+    @PostMapping("/createUser")
+    public User createUser(@Valid @RequestBody User user, HttpServletResponse response){
+        User userNew = userService.createUser(user);
+        response.setHeader("Location", "/users/" + user.getId());
+        return userNew;
+    }
 }
