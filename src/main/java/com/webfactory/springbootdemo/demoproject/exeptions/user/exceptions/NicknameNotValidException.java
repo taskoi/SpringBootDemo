afbeba6cdca5501c7e9601022f13888550d92b2f
ivan.tasevski <ivan.tasevski@webfactory.mk>
2019-06-11 10:58:55 +0200

    fixed bug with ExceptionHandler

--- src/main/java/com/webfactory/springbootdemo/demoproject/exeptions/user/exceptions/NicknameNotValidException.java
+++ src/main/java/com/webfactory/springbootdemo/demoproject/exeptions/user/exceptions/NicknameNotValidException.java
@@ -1,10 +1,9 @@
 package com.webfactory.springbootdemo.demoproject.exeptions.user.exceptions;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 
-public class NicknameNotValidException extends Exception {
+
+public class NicknameNotValidException extends RuntimeException {
     public NicknameNotValidException(String s){
         super(s);
     }
